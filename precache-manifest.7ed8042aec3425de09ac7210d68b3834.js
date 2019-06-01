9d297ae797d224712161a5f53eff6d378f348010
kamesas <semakaleksandr2014@gmail.com>
2019-06-01 10:12:21 +0300

    Updates

---
+++ precache-manifest.7ed8042aec3425de09ac7210d68b3834.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "858cf3efac01ff9c1f43d535ed23f13f",
+    "url": "/git-hub-page/index.html"
+  },
+  {
+    "revision": "696cf0ae2a120ecb7f66",
+    "url": "/git-hub-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/git-hub-page/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "696cf0ae2a120ecb7f66",
+    "url": "/git-hub-page/static/js/main.d173497c.chunk.js"
+  },
+  {
+    "revision": "30b27546f07df50d44a5",
+    "url": "/git-hub-page/static/js/runtime~main.0c80f4e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-hub-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
